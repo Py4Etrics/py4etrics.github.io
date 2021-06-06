3c89a25fb1d68549cef6f2950f90c562a229b5e8
Tetsu Haruyama <tetsu.yes@gmail.com>
2021-06-06 13:47:08 +0900

    py4macro

---
+++ _build/jupyter_execute/py4macro.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Pythonで学ぶ中級マクロ経済学
+# 
+# [Pythonで学ぶ中級マクロ経済学](https://py4macro.github.io/)
+# 
+# 第１部：Pythonの学習
+# 
+# [　　Pythonの基礎](https://py4macro.github.io/1_Python_Basics.html)
+# 
+# [　　NumPy](https://py4macro.github.io/2_NumPy.html)
+# 
+# [　　Pandas](https://py4macro.github.io/3_Pandas.html)
+# 
+# [　　Matplotlib](https://py4macro.github.io/4_Matplotlib.html)
+# 
+# 第２部：マクロ経済学 - 長期分析
+# 
+# [　　発展会計と成長会計](https://py4macro.github.io/5_Income_Differences.html)
+# 
+# [　　回帰分析](https://py4macro.github.io/6_Regression.html)
+# 
+# [　　差分方程式と経済分析](https://py4macro.github.io/7_DifferenceEq.html)
+# 
+# [　　ソロー・モデル](https://py4macro.github.io/8_Solow.html)
+# 
+# [　　所得分布と所得収斂](https://py4macro.github.io/9_Convergence.html)
+# 
+# 第３部：マクロ経済学 - 短期分析
+# 
+# [　　GDPと構成要素](https://py4macro.github.io/10_Data_Fluctuation.html)
+# 
+# [　　今後の計画](https://py4macro.github.io/Plan.html)
+# 
+# 第４部：参考
+# 
+# [　　Pandas：プロット](https://py4macro.github.io/Pandas_plot.html)
+# 
+# [　　解の求め方と最適化問題](https://py4macro.github.io/ScipyOptimize.html)
+# 
+# [　　Tipsと注意点](https://py4macro.github.io/things_to_note.html)
+# 
+# [　　成績分析](https://py4macro.github.io/Grade.html)
